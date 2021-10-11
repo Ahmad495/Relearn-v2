0fae5c6b0b9a928b16f2874e4ecdffa31ff22cf7
Ahmad495 <amdagha01@gmail.com>
2021-10-11 22:37:18 +0500

    implemented custome form validation

--- serverSideValidation.js
+++ serverSideValidation.js
@@ -5,6 +5,7 @@ module.exports.userRegisterValidation = joi.object({
         email: joi.string().required(),
         username: joi.string().required(),
         userType: joi.string().required(),
-        password: joi.string().required()
+        password: joi.string().required(),
+        passwordCheck: joi.string().required()
     }).required()
 })
